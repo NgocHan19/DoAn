2e05d4bcb2bb71594b0b729ecb6c2b4a1f167a4b
NgocHan1011 <145275015+NgocHan1011@users.noreply.github.com>
2023-10-04 23:00:31 +0700

    Add project files.

---
+++ ĐồÁn_LT_Bookstores/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ĐồÁn_LT_Bookstores
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ĐồÁn_LT_Bookstores/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ĐồÁn_LT_Bookstores.MvcApplication" Language="C#" %>
